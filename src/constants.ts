d9978231b069e9033c7b75f53d7889c087b55503
suretrust <saholadele@gmail.com>
2025-05-30 11:52:52 +0200

    feat: add constants and update css

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_PAGE_PATH = '/';
+export const LOADER_TEXT = 'popping your popcorn';
+export const MOVIE_SEARCH_PARAM = 'search';
+export const NAV_ITEMS = [
+  { name: 'movie search', path: '/' },
+  { name: 'favorites', path: '/favorites' },
+];
